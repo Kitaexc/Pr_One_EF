df24428f782efcd709ccc1608cb0831441386fd4
Kitaexc <isip_a.p.chebotarev@mpt.ru>
2024-03-30 00:34:20 +0300

    Pr_One_EF

---
+++ Praktica_EF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktica_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Str_One_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page1();
+        }
+
+        private void Str_two_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page2();
+        }
+
+        private void Str_three_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page3();
+        }
+    }
+}
---
+++ Praktica_EF/Page2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktica_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        private UNLV_STOREEntities context = new UNLV_STOREEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            Products.ItemsSource = context.Products.ToList();
+        }
+    }
+}
